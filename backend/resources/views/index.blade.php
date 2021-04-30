bd51eafcaa0dd8d38b4243cde8b35b56c5c63653
skonishi1125 <skonishi1125@gmail.com>
2021-05-01 08:12:25 +0900

    Auth導入

---
+++ backend/resources/views/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-header">
+          <h3>かあスレッド</h3>
+        </div>
+
+        <div class="card-body">
+          <h2>h2タイトル</h2>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+<!-- jsを読み込むときは、backend/publicのパス記述を省略させる -->
+<script type="text/javascript" src="{{ asset('js/jquery-3.5.1.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/test.js') }}"></script>
+
+@endsection
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function() {
+  return view('index');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
