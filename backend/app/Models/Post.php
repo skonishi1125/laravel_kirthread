2cdda9e87190a9c68b4aa86b894f86131b101c33
skonishi1125 <skonishi1125@gmail.com>
2021-06-17 01:03:55 +0900

    リアクション機能を追加

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Auth;
 
 class Post extends Model
 {
@@ -13,6 +14,16 @@ class Post extends Model
 
     public function makeLink($value) {
         return mb_ereg_replace("(https?)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)" , '<a href="\1\2">\1\2</a>' , $value);
+    }
+
+    public function isSetReaction($user_id, $post_id, $reaction_number = '') {
+      $is_set_reaction = Reaction::where('user_id', $user_id)
+          ->where('post_id', $post_id)
+          ->where('reaction_number', $reaction_number)
+          ->exists();
+      // ログイン中のユーザが、その投稿に、同じリアクションをつけているかどうか。
+      // true = つけている。 false = つけていない。
+      return $is_set_reaction;
     }
 
 }
--- backend/public/js/script.js
+++ backend/public/js/script.js
@@ -5,9 +5,8 @@
     var trash = document.getElementsByClassName('trash');
     // console.log(trash[0]);
     // console.log(trash[0].href);
-
     var i;
-    for ( i = 0; i < trash.length; i++ ) {
+    for ( let i = 0; i < trash.length; i++ ) {
         trash[ i ].addEventListener( 'click', function( e ) {
             // e.preventDefault();
             // console.log(trash[i]); は動作しない
@@ -23,6 +22,38 @@
             }
         }, false );
     }
-  }, false );
 
+    // リアクションを押したときの反応
+    let reaction = document.getElementsByClassName('reaction');
+    console.log(reaction);
+    for ( let i = 0; i < reaction.length; i++) {
+      reaction[ i ].addEventListener('click', (e) => {
+        // クリックしたreactionの初めの要素を取得している。これをクリックした要素に直す
+        // console.log(document.querySelectorAll('.reaction'));
+        
+        // console.log(this.querySelectorAll('.reaction'));
+        // console.log(this);
+        // 上の場合、thisが#documentになっている
+
+        // クリックした要素の選択
+        console.log(reaction[i]);
+
+        // クリックした親を取得して、その兄弟要素のreaction_modalを表示する
+        let oya = reaction[i].parentNode;
+        console.log(oya);
+
+        let eventDOM = e.target;
+        let eventParent = e.target.parentNode;
+        let eventParentNextSib = eventParent.nextElementSibling;
+        console.dir(eventDOM);
+        console.dir(eventParent);
+        console.log(eventParentNextSib);
+        eventParentNextSib.classList.toggle('d-none');
+
+        // 最初に出てきた(querySelector)では取得できているので、これをクリックした要素にしたい・・・
+        console.log(document.querySelector('.reaction').parentNode.nextElementSibling);
+      })
+    };
+
+  }, false ); // DOMContentLoaded
 }
