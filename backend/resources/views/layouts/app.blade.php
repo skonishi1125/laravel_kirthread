0f138bf3699c63959b42e6ec32039b0c31594f3c
skonishi1125 <skonishi1125@gmail.com>
2023-11-04 23:14:09 +0900

    環境が増えたのでブラウザタブのtitleで見分けがつくようにした。

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -7,7 +7,12 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>
+      @if  (config('app.env') !== 'production')
+        {{ config('app.env') }}
+      @endif
+      {{ config('app.name', 'Laravel') }}
+    </title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
