994fa6a351aec14041db67572efefeb565dcc01a
skonishi1125 <skonishi1125@gmail.com>
2024-08-24 17:03:52 +0900

    こっそりバージョンも更新しておこう

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
       <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
           {{ config('app.name', 'Laravel') }}
-          <small>v1.2</small>
+          <small>v2.0</small>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
