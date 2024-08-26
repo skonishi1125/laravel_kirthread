fa06b538abe67303bce4d5bbda5237d46811561b
skonishi1125 <skonishi1125@gmail.com>
2024-08-26 23:43:51 +0900

    ビルドして、本番環境でもvue3.4が動作するか確認してみる

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -8,9 +8,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // ただしこれはapp.jsの範囲でjQueryを書けるだけで、
 // その他ファイルでjQueryを使う場合は都度importする必要がある。
 import $ from 'jquery';
-// $(document).ready(function() {
-//   console.log('jQuery is working!');
-// });
+$(document).ready(function() {
+  console.log('vue3.4 and jQuery check');
+});
 
 // 汎用のjsファイルを読み込む
 // game/panel.jsはそのページでしか使わないので、そちらで読み込ませる。
