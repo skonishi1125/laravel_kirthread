1dd460a0ecc926b1da57514da8433f184d10b8ec
skonishi1125 <skonishi1125@gmail.com>
2024-09-01 02:21:21 +0900

    メニュー画面の仮レイアウト調整

--- backend/resources/js/components/RPG/Menu.vue
+++ backend/resources/js/components/RPG/Menu.vue
@@ -18,7 +18,10 @@
       <div><button @click="$router.push('/game/rpg/menu/skill')">スキル振り</button></div>
       <div><button @click="endGame">タイトルに戻る</button></div>
     </div>
-    <div class="col-9">
+    <div class="col-9" v-if="isMenuRoute">
+      <p>あなたは街に帰ってきました。</p>
+    </div>
+    <div v-else class="col-9">
       <!-- メニューはそのまま、children固有の要素を出す -->
       <router-view></router-view>
     </div>
@@ -28,11 +31,19 @@
 
 <script>
 export default {
+  computed: {
+    // methodsに書かずcomputedに書く。
+    // リアクティブに監視されるため、URLが変更された時に自動的に再計算される
+    isMenuRoute() {
+      return this.$route.path == '/game/rpg/menu';
+    },
+  },
   created() {
     this.$store.dispatch('setScreen', 'menu');
   },
   mounted() {
     console.log('Menu.vue');
+    console.log(this.isMenuRoute);
   },
   methods: {
     endGame() {
