7e72322ebfd76cf0dcb958efb744215c87fa6ed7
skonishi1125 <skonishi1125@gmail.com>
2025-04-23 01:14:32 +0900

    所持金を400円

--- backend/app/Models/Game/Rpg/Savedata.php
+++ backend/app/Models/Game/Rpg/Savedata.php
@@ -13,8 +13,8 @@ class Savedata extends Model
 {
     use HasFactory;
 
-    // データ作成時のデフォルトの所持金
-    public const DEFAULT_MONEY = 300;
+    // データ作成時のデフォルトの所持金 どうせ貯まるので、多めに持たせてやろう
+    public const DEFAULT_MONEY = 400;
 
     protected $table = 'rpg_savedatas';
 
