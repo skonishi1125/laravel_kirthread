9aa7a537af41d2a4c641090f7c0ff8d15ace3c82
skonishi1125 <skonishi1125@gmail.com>
2025-04-24 01:36:54 +0900

    未ログイン者が、game/rpg/statusなどにアクセスできなくした

--- backend/app/Http/Controllers/Game/Rpg/IndexController.php
+++ backend/app/Http/Controllers/Game/Rpg/IndexController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\Game\Rpg;
 
 use App\Http\Controllers\Controller;
+use Auth;
 
 class IndexController extends Controller
 {
     public function index()
     {
+        // 未ログインユーザーがメニュー画面などにURL直打ちでアクセスした場合は、トップにリダイレクトさせる
+        if (! Auth::check()) {
+            if (request()->path() !== 'game/rpg') {
+                return redirect('/game/rpg');
+            }
+        }
+
         return view('game/rpg/index');
     }
 }
