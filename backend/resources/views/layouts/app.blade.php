82214b00b22820d006d4136df0162973ce1e4606
skonishi1125 <skonishi1125@gmail.com>
2025-11-20 17:50:43 +0800

    タブ増やした

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -119,9 +119,9 @@
                 </a>
 
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="{{ route('guest_upgrade_get') }}">
-                      本登録
-                  </a>
+                  <a class="dropdown-item" href="{{ route('guest_upgrade_get') }}">本登録</a>
+                  <a class="dropdown-item" href="{{ route('game') }}">ゲーム</a>
+                  <a class="dropdown-item" href="{{ route('about') }}">かあスレッドとは</a>
                   <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                     {{ __('Logout') }}
                   </a>
