b08ca442904e8a13d91ab963cafca9e4f7ddd18a
skonishi1125 <skonishi1125@gmail.com>
2025-11-21 21:59:26 +0800

    もうちょっと調整

--- backend/resources/views/config/index.blade.php
+++ backend/resources/views/config/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container my-5">
+<div class="container my-3">
   <div class="row">
     <div class="col-md-12">
       <div class="card">
