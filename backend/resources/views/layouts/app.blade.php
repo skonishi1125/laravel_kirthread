02adaa0ff9b030a82ffabb803dec7de248af7605
skonishi <73050501+skonishi1125@users.noreply.github.com>
2025-11-21 15:25:03 +0800

    251121 adjust touch device 2 (#118)
    * plaza.vueを、タッチ端末でもある程度触りやすくした
    
    * スキルのmouseoverもスマホ対応
    
    * pushし忘れた

--- backend/resources/views/layouts/app.blade.php
+++ backend/resources/views/layouts/app.blade.php
@@ -70,7 +70,8 @@
 
 <body>
   <div id="app">
-    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    {{-- RPG スマホ画面のケースのヘッダーずれ対応 --}}
+    <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm {{ Request::is('game/rpg*') ? 'is-rpg' : '' }}">
       <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
           {{ config('app.name') }}
