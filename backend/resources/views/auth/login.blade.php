6ae41afc454cca3f57c083cf58cf6b2b6cb08bac
skonishi1125 <skonishi1125@gmail.com>
2025-11-23 11:07:52 +0800

    remembermeをデフォルトでtrueとした

--- backend/resources/views/auth/login.blade.php
+++ backend/resources/views/auth/login.blade.php
@@ -46,7 +46,7 @@
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }} checked>
 
                                     <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
